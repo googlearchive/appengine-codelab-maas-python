a4a3e4650022df9799e425cc11c273306ef6f2ba
Takashi Matsuo <tmatsuo@google.com>
2013-08-29 11:49:16 -0700

    Added another font for completing the font set.

--- main.py
+++ main.py
@@ -18,10 +18,21 @@ MEME_TEMPLATES = {
     'happyboy': 'happyboy.jpeg',
     'rapperboy': 'rapperboy.jpeg',
     'realappengine': 'realappengine.jpeg',
+    'shrugging': 'shrugging.jpeg',
 }
 
 DEFAULT_TEMPLATE = 'happyboy.jpeg'
 
+IMAGE_DIR = 'images'
+
+MEME_FONTS = {
+    'FreeSerif': 'FreeSerif.ttf',
+    'mikachan': 'mikachan.ttf',
+}
+
+DEFAULT_FONT = 'FreeSerif.ttf'
+
+FONT_DIR = 'fonts'
 
 class MainHandler(webapp2.RequestHandler):
     """A handler for showing an HTML form."""
@@ -29,7 +40,8 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         """Render an HTML form for creating Memes."""
         template = JINJA_ENV.get_template('index.html')
-        context = {'templates': sorted(MEME_TEMPLATES.keys())}
+        context = {'templates': sorted(MEME_TEMPLATES.keys()),
+                   'fonts': sorted(MEME_FONTS.keys())}
         self.response.out.write(template.render(context))
 
 
@@ -43,7 +55,11 @@ class ImageHandler(webapp2.RequestHandler):
             A rendered Image object.
         """
         meme_template = self.request.get('meme_template')
-        image = Image.open(MEME_TEMPLATES.get(meme_template, DEFAULT_TEMPLATE))
+        image = Image.open(os.path.join(IMAGE_DIR,
+            MEME_TEMPLATES.get(meme_template, DEFAULT_TEMPLATE)))
+        meme_font = self.request.get('meme_font')
+        font_file = os.path.join(FONT_DIR,
+                                 MEME_FONTS.get(meme_font, DEFAULT_FONT))
         upper_text = self.request.get('upper_text')
         upper_text_align = self.request.get('upper_text_align', meme.MIDDLE)
         middle_text = self.request.get('middle_text')
@@ -51,11 +67,14 @@ class ImageHandler(webapp2.RequestHandler):
         lower_text = self.request.get('lower_text')
         lower_text_align = self.request.get('lower_text_align', meme.MIDDLE)
         if upper_text:
-            meme.draw_text(image, meme.TOP, upper_text_align, upper_text)
+            meme.draw_text(image, meme.TOP, upper_text_align, upper_text,
+                           font_file=font_file)
         if middle_text:
-            meme.draw_text(image, meme.MIDDLE, middle_text_align, middle_text)
+            meme.draw_text(image, meme.MIDDLE, middle_text_align, middle_text,
+                           font_file=font_file)
         if lower_text:
-            meme.draw_text(image, meme.BOTTOM, lower_text_align, lower_text)
+            meme.draw_text(image, meme.BOTTOM, lower_text_align, lower_text,
+                           font_file=font_file)
         return image
 
     def get(self):
