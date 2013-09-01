607ad5598ccc1b2d9f2ef45e45a65c81a53247cc
Takashi Matsuo <tmatsuo@google.com>
2013-08-31 18:12:59 -0700

    Moved index.html to the templates directory.

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ import meme
 
 
 JINJA_ENV = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), 'templates')),
     extensions=['jinja2.ext.autoescape'])
 
 MEME_TEMPLATES = collections.OrderedDict([
